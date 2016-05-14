7b7f3565efa241fc8c28c97d68e91c9b40c78ed4
fanghui <fh006007@163.com>
2016-05-14 10:11:37 +0800

    init

---
+++ Relation_auto/src/utils/HkmeanArrData.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import main.FinalMain;
+import spectralclustering.Adjacentmatrix;
+import spectralclustering.MatrixOpera;
+
+public class HkmeanArrData {
+  
+    
+    public static void outputKarrData() {
+    	
+	}
+    
+}
